28decd460b0251c545542b80b466f7e357e794d1
Yehuda Hason <yehudaxt@gmail.com>
2026-04-23 12:44:02 +0300

    final

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // exact coordinates for 99 King St in Newport, RI 02840
-
-var map = L.map("map").setView([41.4820815, -71.3116523], 15);
+// 41.481469, -71.310429
+var map = L.map("map").setView([41.481469, -71.310429], 15);
 
 // 2. Add the OpenStreetMap tiles
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -9,7 +9,7 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 // 3. Add a marker for 99 King Street
-L.marker([41.4820815, -71.3116523])
+L.marker([41.481469, -71.310429])
   .addTo(map)
   .bindPopup("<b>99 King Street</b><br>Newport, RI 02840")
   .openPopup();
